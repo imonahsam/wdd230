19b430fe380c0f0763cf1474635055eb37ba8975
imonahsam <samimonah@gmail.com>
2024-03-04 11:30:31 +0100

    Home page and site plan

---
+++ scripts/getDates.js
@@ -0,0 +1,20 @@
+const modified = document.getElementById('last-modified');
+modified.style.color = 'white';
+
+let lastMod = new Date(document.lastModified);
+
+modified.innerHTML += lastMod;
+
+const visitsDisplay = document.getElementById("visit");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.innerHTML += numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
