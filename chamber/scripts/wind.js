9eb349404a14b4772ba7a847b1bfe775d241927c
imonahsam <samimonah@gmail.com>
2024-06-01 20:48:19 +0100

    displaying names of city

--- chamber/scripts/wind.js
+++ chamber/scripts/wind.js
@@ -22,6 +22,7 @@ async function apiFetch() {
     }
 }
 
+
 apiFetch();
 
 function displayWeather(data) {
--- project/scripts/script.js
+++ project/scripts/script.js
@@ -1,7 +1,7 @@
 function getWeather() {
     const apiKey = `1f0b212ea14233b8318cb6b2e4943be2`;
     const city = document.getElementById('city').value;
-    
+
     if (!city) {
         alert('Please enter a city name');
         return;
@@ -14,21 +14,21 @@ function getWeather() {
         .then(response => response.json())
         .then(data => {
             displayWeather(data);
-    })
-    .catch(error => {
-        console.error('Error fetching current weather data:', error);
-        alert('Error fetching current weather data, Please try again.');
-    });
+        })
+        .catch(error => {
+            console.error('Error fetching current weather data:', error);
+            alert('Error fetching current weather data, Please try again.');
+        });
 
     fetch(forecastUrl)
         .then(response => response.json())
         .then(data => {
             displayHourlyForecast(data.list);
-    })
-    .catch(error => {
-        console.error('Error fetching hourly forecast data:', error);
-        alert('Error fetching hourly forecast data, Please try again.');
-    });
+        })
+        .catch(error => {
+            console.error('Error fetching hourly forecast data:', error);
+            alert('Error fetching hourly forecast data, Please try again.');
+        });
 }
 
 function displayWeather(data) {
@@ -36,7 +36,7 @@ function displayWeather(data) {
     const weatherInfo = document.getElementById('weather-info');
     const weatherIcon = document.getElementById('weather-icon');
     const hourlyForcast = document.getElementById('hourly-forcast');
-    
+
     // Clear previous content
     weatherInfo.innerHTML = '';
     hourlyForcast.innerHTML = '';
@@ -70,7 +70,7 @@ function displayHourlyForecast(hourlyData) {
     const hourlyForcast = document.getElementById('hourly-forcast');
     const next24Hours = hourlyData.slice(0, 8);
 
-    next24Hours.foreach(item => {
+    next24Hours.forEach(item => {
         const dateTime = new Date(item.dt * 1000);
         const hour = dateTime.getHours();
         const temperature = Math.round(item.main.temp - 273.15);
@@ -85,11 +85,10 @@ function displayHourlyForecast(hourlyData) {
             </div>
         `;
         hourlyForcast.innerHTML += hourItemHtml;
-            
     });
+}
 
-    function showImage() {
-        const weatherIcon = document.getElementById('weather-icon');
-        weatherIcon.style.display = 'block';
-    }
+function showImage() {
+    const weatherIcon = document.getElementById('weather-icon');
+    weatherIcon.style.display = 'block';
 }
